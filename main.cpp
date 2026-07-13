b47e11af946ce576eb2532e83d395aed93e0fde0
daratarasevic <tarasevish80@gmail.com>
2026-07-14 00:47:49 +0300

    it handles KeywordDef now

--- main.cpp
+++ main.cpp
@@ -43,6 +43,44 @@ int main() {
                 double value = evaluator.evaluate(rpn, environment);
 
                 environment.setVariable(varName, value);
+            } else if (tokens[0].type == TokensType::KeywordDef) {
+                if (tokens.size() < 7 || tokens[1].type != TokensType::Variable || tokens[2].type != TokensType::LeftParen) {
+                    throw std::runtime_error("Syntax error in function declaration.");
+                }
+
+                std::string funcName = tokens[1].value;
+                std::vector<std::string> parameters;
+
+                int i = 3;
+                while (i < tokens.size() && tokens[i].type != TokensType::RightParen) {
+                    if (tokens[i].type == TokensType::Variable) {
+                        parameters.push_back(tokens[i].value);
+                    }
+                    i++;
+                }
+
+                while (i < tokens.size() && tokens[i].type != TokensType::LeftCurly) {
+                    i++;
+                }
+
+                if (i >= tokens.size()) {
+                    throw std::runtime_error("Missing '{'!");
+                }
+                i++;
+
+                std::vector<Token> functionExpression;
+                while (i < tokens.size() && tokens[i].type != TokensType::RightCurly) {
+                    functionExpression.push_back(tokens[i]);
+                    i++;
+                }
+
+                if (i >= tokens.size()) {
+                    throw std::runtime_error("Missing '}'!");
+                }
+
+                auto rnpFunction = shunting_yard.parseToRPN(functionExpression);
+                environment.setFunction(FunctionSymbol{funcName, parameters, rnpFunction});
+
             } else {
                 auto rpn = shunting_yard.parseToRPN(tokens);
                 double valueResult = evaluator.evaluate(rpn, environment);
