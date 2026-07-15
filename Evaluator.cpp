ded5e7de092e878473d819d1f56815daaa3378a1
daratarasevic <tarasevish80@gmail.com>
2026-07-15 14:11:30 +0300

    add integral evaluation

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -7,8 +7,22 @@ double Evaluator::evaluate(const std::vector<Token> &rpnTokens, const Environmen
     for (const auto &token: rpnTokens) {
         if (token.type == TokensType::Number) {
             valuesStack.push(std::stod(token.value)); //String TO Double
-        } else if (token.type == TokensType::Variable) {
+        }
+
+        else if (token.type == TokensType::Variable) {
             if (env.hasFunction(token.value)) {
+
+                bool hasIntegral = false;
+                for (const auto& t : rpnTokens) {
+                    if (t.value == "integral") {
+                        hasIntegral = true;
+                    }
+                }
+
+                if (hasIntegral) {
+                    continue;
+                }
+
                 FunctionSymbol func = env.getFunction(token.value);
 
                 if (valuesStack.size() < func.parameters.size()) {
@@ -32,7 +46,9 @@ double Evaluator::evaluate(const std::vector<Token> &rpnTokens, const Environmen
             } else {
                 valuesStack.push(env.getVariable(token.value));
             }
-        } else if (token.type == TokensType::Operator) {
+        }
+
+        else if (token.type == TokensType::Operator) {
             if (valuesStack.size() < 2) {
                 throw std::runtime_error("Invalid expression.");
             }
@@ -55,7 +71,9 @@ double Evaluator::evaluate(const std::vector<Token> &rpnTokens, const Environmen
                     valuesStack.push(a / b);
                 }
             }
-        } else if (token.type == TokensType::FunctionName) {
+        }
+
+        else if (token.type == TokensType::FunctionName) {
             if (token.value == "abs") {
                 if (valuesStack.empty()) {
                     throw std::runtime_error("Stack is empty.");
@@ -64,7 +82,7 @@ double Evaluator::evaluate(const std::vector<Token> &rpnTokens, const Environmen
                 valuesStack.pop();
 
                 valuesStack.push(std::abs(a));
-            } else if (token.value == "max" || token.value == "min" || token.value == "pow") {
+            } else if (token.value == "max" || token.value == "min" || token.value == "pow" || token.value == "integral") {
                 if (valuesStack.size() < 2) {
                     throw std::runtime_error("Invalid expression.");
                 }
@@ -81,6 +99,46 @@ double Evaluator::evaluate(const std::vector<Token> &rpnTokens, const Environmen
                     valuesStack.push(std::min(a, b));
                 } else if (token.value == "pow") {
                     valuesStack.push(std::pow(a, b));
+                } else if (token.value == "integral"){
+
+                    int n = 1000;
+                    double dx = (b-a)/n;
+                    double sum = 0.0;
+
+                    std::string funcName = "";
+                    for (const auto& t : rpnTokens) {
+                        if (t.type == TokensType::Variable && env.hasFunction(t.value)) {
+                            funcName = t.value;
+                            break;
+                        }
+                    }
+
+                    if (funcName.empty()) {
+                        throw std::runtime_error("No function provided for integral.");
+                    }
+
+                    FunctionSymbol targetFunc = env.getFunction(funcName);
+
+                    std::string paramName = targetFunc.parameters[0];
+
+                    Environment envA = env;
+                    envA.setVariable(paramName, a);
+                    double fa = evaluate(targetFunc.RPNtokens, envA);
+
+                    Environment envB = env;
+                    envB.setVariable(paramName, b);
+                    double fb = evaluate(targetFunc.RPNtokens, envB);
+
+                    for (int i = 1; i < n; ++i) {
+                        double xi = a + i * dx;
+
+                        Environment tempEnv = env;
+                        tempEnv.setVariable(paramName, xi);
+                        sum += evaluate(targetFunc.RPNtokens, tempEnv);
+                    }
+
+                    double trapezoidalRuleResult = dx * (((fa + fb) / 2.0) + sum); // за формулою трапецій: півсума основ крайніх точок + сума всіх внутрішніх точок, помножене на ширину dx
+                    valuesStack.push(trapezoidalRuleResult);
                 }
             }
         }
